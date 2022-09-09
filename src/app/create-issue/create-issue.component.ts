774f749c7c51892ec17231e68391fd62284e033e
= <=ironhack082327@gmail.com>
2022-09-09 12:08:06 +0530

    added request for create issue

--- src/app/create-issue/create-issue.component.ts
+++ src/app/create-issue/create-issue.component.ts
@@ -7,6 +7,23 @@ import axios from 'axios';
 })
 export class CreateIssueComponent implements OnInit {
   title = "Hello"
+  projects:any = []
+  users:any = []
+  get_all_users(){
+    axios.get("https://hu-22-angular-mockapi-urtjok3rza-wl.a.run.app/user").then(res=>{
+      this.users = res.data;
+    }).catch(err=>{
+      console.error(err)
+    })
+  }
+  get_all_projects(){
+    axios.get("https://hu-22-angular-mockapi-urtjok3rza-wl.a.run.app/project").then(result=>{
+      console.log(result)
+      this.projects = result.data
+    }).catch(err=>{
+      console.log(err);
+    })
+  }
   constructor() { }
   onFormSubmit(){
     // axios.post("https://hu-22-angular-mockapi-urtjok3rza-wl.a.run.app/issue",{data:{}}).then(res=>{
@@ -17,8 +34,21 @@ export class CreateIssueComponent implements OnInit {
 
 
   }
+  createNewIssue(data:any){
+    data.status = 1; //as not in form hence default status assumption
+    data.type = parseInt(data.type)
+    console.log(data)
+    axios.post("https://hu-22-angular-mockapi-urtjok3rza-wl.a.run.app/issue",data).then(res=>{
+      console.log(res);
+      alert("Created new issue")
+    }).catch(err=>{
+      console.error(err)
+    })
+  }
   ngOnInit(): void {
     this.onFormSubmit()
+    this.get_all_projects()
+    this.get_all_users()
   }
 
 }
